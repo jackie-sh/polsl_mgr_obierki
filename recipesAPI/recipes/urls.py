e4c5f2a69fd7bbcb3f311d1c5361630bf7c3c3ca
rainsha <aklinaoo@gmail.com>
2021-12-28 21:12:03 +0100

    Recipes mock endpoints (TO REDO cause swagger not liking that)

--- recipesAPI/recipes/urls.py
+++ recipesAPI/recipes/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from recipes import views
 
 urlpatterns = [
-      #path('create-recipe', views.getallUsersApi),
+    path('get-recipe/<int:id>/', views.get_recipe),
+    path('create-recipe/', views.create_recipe),
+    path('upload-main-image/<int:id>/', views.upload_main_image),
+    path('edit-recipe/<int:id>/', views.edit_recipe),
+    path('delete-recipe/<int:id>/', views.delete_recipe),
+    path('get-all/', views.get_all),
+    path('get-image/<int:id>/', views.get_image),
+    path('create-comment/<int:id>/', views.create_comment),
 ]
--- recipesAPI/recipes/views.py
+++ recipesAPI/recipes/views.py
@@ -1,3 +1,30 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+
+
+def get_recipe(request, id):
+    return JsonResponse({'temp': 'Mocked'}, status=status.HTTP_200_OK)
+
+def create_recipe(request):
+    return JsonResponse({'temp': 'Mocked'}, status=status.HTTP_200_OK)
+
+def upload_main_image(request, id):
+    return JsonResponse({'temp': 'Mocked'}, status=status.HTTP_200_OK)
+
+def edit_recipe(request,id):
+    return JsonResponse({'temp': 'Mocked'}, status=status.HTTP_200_OK)
+
+def delete_recipe(request,id):
+    return JsonResponse({'temp': 'Mocked'}, status=status.HTTP_200_OK)
+
+def get_all(request):
+    return JsonResponse({'temp': 'Mocked'}, status=status.HTTP_200_OK)
+
+def get_image(request, id):
+    return JsonResponse({'temp': 'Mocked'}, status=status.HTTP_200_OK)
+
+def create_comment(request, id):
+    return JsonResponse({'temp': 'Mocked'}, status=status.HTTP_200_OK)
