42078e2bac8be37ef0c5defe1f22440d5c927f70
Michal Jankowski <michaljankowskiReal@gmail.com>
2021-12-31 03:14:39 +0100

    Move message model to recipesAPI

--- recipesAPI/recipes/models.py
+++ recipesAPI/recipes/models.py
@@ -29,3 +29,17 @@ class Recipe(models.Model):
 
     class Meta:
         ordering = ['recipe_type', 'view_count']
+
+
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    message = models.CharField(max_length=300)
+    pub_date = models.DateField()
+    senderID = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
+    receiverID = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver")
+
+    def __str__(self):
+        return "%s %s %s" % (self.message, self.senderID, self.receiverID)
+
+    class Meta:
+        ordering = ['message', 'pub_date']
