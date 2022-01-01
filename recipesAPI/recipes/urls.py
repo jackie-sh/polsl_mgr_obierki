6d967a7ec997ec98fd9045d01b49941371122ad6
rainsha <aklinaoo@gmail.com>
2022-01-01 18:55:08 +0100

    recipe get

--- recipesAPI/recipes/urls.py
+++ recipesAPI/recipes/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path('get-recipe/<int:pk>/', views.RecipeDetails.as_view()),
-    path('create-recipe/', views.RecipeView.as_view(http_method_names=['post'])),
+    path('get-recipe/<int:pk>/', views.RecipeGetView.as_view(http_method_names=['get'])),
+    path('create-recipe/', views.RecipeCreateView.as_view(http_method_names=['post'])),
     path('upload-main-image/<int:pk>/', views.upload_main_image),
     path('edit-recipe/<int:pk>/',  views.RecipeDetails.as_view()),
     path('delete-recipe/<int:pk>/', views.RecipeDetails.as_view()),
--- recipesAPI/recipes/views.py
+++ recipesAPI/recipes/views.py
@@ -33,39 +33,6 @@ def create_comment(request, id):
 
 class RecipeDetails(APIView):
 
-    def get_message_object(self, pk):
-        try:
-            return Message.objects.get(pk=pk)
-        except Message.DoesNotExist:
-            raise Http404
-
-    def get_recipe_object(self, pk):
-        try:
-            return Recipe.objects.get(pk=pk)
-        except Recipe.DoesNotExist:
-            raise Http404
-
-    #@swagger_auto_schema(tags=["recipe"])
-    def get(self, request, pk, format=None):
-        try:
-            recipe = self.get_recipe_object(pk)
-        except Http404:
-            return JsonResponse({'isUpdated': False, 'errorMessage': "Recipe does not exist"}, safe=False,
-                                status=status.HTTP_404_NOT_FOUND)
-        recipe_serializer = RecipeSerializer(recipe, data=request.data)
-        try:
-            if recipe_serializer.is_valid(raise_exception=True):
-                recipe_serializer['views'] += 1
-                recipe_serializer.save()
-                recipe_modified = self.get_recipe_object(pk)
-                recipe_serializer_modified = RecipeSerializer(recipe, data=request.data)
-                messsage = self.get_message_object(pk)
-                # TODO: Get list of messages and attach to recipe, calculate mean of recipe from comments
-                return JsonResponse(recipe_serializer_modified.data, safe=False, status=status.HTTP_200_OK)
-        except serializers.ValidationError as valEr:
-            return JsonResponse({'isUpdated': False, 'errorMessage': valEr.detail}, safe=False,
-                                status=status.HTTP_400_BAD_REQUEST)
-
     #@swagger_auto_schema(tags=["recipe"])
     def put(self, request, pk, format=None):
         try:
@@ -93,7 +60,38 @@ class RecipeDetails(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class RecipeView(GenericAPIView):
+class RecipeGetView(GenericAPIView):
+    serializer_class = RecipeSerializer
+
+    @swagger_auto_schema(tags=["recipe"],
+     responses={
+         status.HTTP_200_OK: Schema(type=TYPE_OBJECT,
+                properties={
+                    'title': Schema(type=TYPE_STRING),
+                    'content': Schema(type=TYPE_STRING),
+                    'authorName': Schema(type=TYPE_STRING),
+                    'recipeId': Schema(type=TYPE_INTEGER),
+                    'authorId': Schema(type=TYPE_INTEGER),
+                    'categoryId': Schema(type=TYPE_INTEGER),
+                    'shortDescription': Schema(type=TYPE_STRING),
+                    'mainImageId': Schema(type=TYPE_INTEGER)
+                })
+         }
+     )
+    def get(self, request, pk, format=None):
+        try:
+            recipe = Recipe.objects.get(pk=pk)
+        except Http404:
+            return JsonResponse({}, status=status.HTTP_404_NOT_FOUND)
+        recipe_serializer = RecipeSerializer(recipe, data={'view_count': recipe.view_count + 1}, partial=True)
+        try:
+            if recipe_serializer.is_valid(raise_exception=True):
+                recipe_serializer.save()
+                return JsonResponse(recipe_serializer.data, safe=False, status=status.HTTP_200_OK)
+        except serializers.ValidationError as valEr:
+            return JsonResponse({'errorMessage': valEr.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+class RecipeCreateView(GenericAPIView):
     serializer_class = RecipeSerializer
 
     @swagger_auto_schema(tags=["recipe"],
