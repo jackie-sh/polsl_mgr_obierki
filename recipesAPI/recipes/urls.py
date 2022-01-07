6caf7a4ff3f00c68af5a759d6847ad54663767e2
Michal Jankowski <michaljankowskiReal@gmail.com>
2022-01-07 21:22:09 +0100

    Add upload-image-id endpoint in accordance with docs

--- recipesAPI/recipes/urls.py
+++ recipesAPI/recipes/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
     path('edit-recipe/<int:pk>',  views.RecipeEditView.as_view(http_method_names=['put'])),
     path('delete-recipe/<int:pk>', views.RecipeDeleteView.as_view(http_method_names=['delete'])),
     path('get-all', views.get_all),
-    path('get-image/<int:pk>', views.RecipeGetImage.as_view(http_method_names=['get'])),
     path('create-comment', views.RecipeCreateCommentView.as_view(http_method_names=['post'])),
     path('get-categories', views.RecipeCategoryView.as_view(http_method_names=['get'])),
+    path('get-image/<int:pk>', views.RecipeGetImage.as_view(http_method_names=['get'])),
+    path('get-image-id/<int:pk>', views.RecipeGetImageByImageId.as_view(http_method_names=['get'])),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
