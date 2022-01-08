71a366580eaa7ef5757524efd6e722b7c8a9529b
Michal Jankowski <michaljankowskiReal@gmail.com>
2022-01-08 21:14:21 +0100

    Change views to float type and restore image upload

--- recipesAPI/profiles/serializers.py
+++ recipesAPI/profiles/serializers.py
@@ -3,5 +3,5 @@ from rest_framework import serializers
 
 class UserProfileSerializer(serializers.Serializer):
     name = serializers.CharField()
-    rating = serializers.IntegerField(allow_null=True)
+    rating = serializers.FloatField(allow_null=True)
     views = serializers.IntegerField(allow_null=True)
