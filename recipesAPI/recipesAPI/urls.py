15dc1c52cb827a89facb7f8dbaca34d9e1c9fd1a
Michal Jankowski <michaljankowskiReal@gmail.com>
2022-01-09 00:21:13 +0100

    Fixes for fixes 2nd part

--- recipesAPI/recipesAPI/urls.py
+++ recipesAPI/recipesAPI/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from django.conf.urls import url, include
+from django.conf.urls import include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from recipesAPI import views
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -30,12 +31,13 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', views.IndexView.as_view()),
     path('admin/', admin.site.urls),
     path('api/auth/', include('rest_framework.urls')),
     path('', include('users.urls'), name="users"),
     path('api/recipes/', include('recipes.urls'), name="recipes"),
     path('api/messages/', include('chat.urls'), name="messages"),
-    path('api/user-profile/', include('profiles.urls'), name="profiles"),
+    path('api/user-profile/', include('profiles.urls'), name="user-profile"),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
