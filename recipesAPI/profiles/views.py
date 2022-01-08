20bce9ddbc728b3b5b781ae809dd6bd66baa432e
Michal Jankowski <michaljankowskiReal@gmail.com>
2022-01-09 00:20:27 +0100

    Fixes for fixes

--- recipesAPI/profiles/views.py
+++ recipesAPI/profiles/views.py
@@ -1,3 +1,101 @@
-from django.shortcuts import render
+from django.db.models import Sum, Avg
+from drf_yasg.openapi import *
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import permissions
+from rest_framework.generics import GenericAPIView
+from recipes.models import *
+from .serializers import *
+from django.http import JsonResponse
+from django.core import serializers
+from rest_framework import status
+from recipes.serializers import RecipeFullViewSerializer
 
-# Create your views here.
+
+class UserProfileGetUserRecipesView(GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = RecipeFullViewSerializer
+
+    @swagger_auto_schema(tags=["userProfile"],
+                         request_body=Schema(
+                             type=TYPE_OBJECT,
+                             properties={
+                                 'userId': Schema(type=TYPE_STRING)
+                             }
+                         ),
+                         responses={
+                             status.HTTP_200_OK: Schema(type=TYPE_OBJECT,
+                                                        properties={
+                                                            'title': Schema(type=TYPE_STRING),
+                                                            'authorName': Schema(type=TYPE_STRING),
+                                                            'recipeId': Schema(type=TYPE_INTEGER),
+                                                            'authorId': Schema(type=TYPE_INTEGER),
+                                                            'shortDescription': Schema(type=TYPE_STRING),
+                                                            'mainImageId': Schema(type=TYPE_INTEGER),
+                                                            'rating': Schema(type=TYPE_INTEGER),
+                                                            'categoryId': Schema(type=TYPE_INTEGER),
+                                                            'createdDate': Schema(type=TYPE_STRING),
+
+                                                        })
+                         }
+                         )
+    def get(self, request, pk):
+        try:
+            user = User.objects.get(pk=pk)
+            userRecipes = Recipe.objects.filter(author=user.id)
+        except (User.DoesNotExist, Recipe.DoesNotExist):
+            return JsonResponse({}, status=status.HTTP_404_NOT_FOUND)
+        serializer = RecipeFullViewSerializer(userRecipes, many=True, fields={
+            'recipeId',
+            'authorId',
+            'categoryId',
+            'shortDescription',
+            'title',
+            'createdDate',
+            'mainImageId',
+            'authorName',
+            'rating'})
+
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
+
+
+class UserProfileGetProfileView(GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserProfileSerializer
+
+    @swagger_auto_schema(tags=["userProfile"],
+                         request_body=Schema(
+                             type=TYPE_OBJECT,
+                             properties={
+                                 'userId': Schema(type=TYPE_STRING)
+                             }
+                         ),
+                         responses={
+                             status.HTTP_200_OK: Schema(type=TYPE_OBJECT,
+                                                        properties={
+                                                            'name': Schema(type=TYPE_STRING),
+                                                            'rating': Schema(type=TYPE_INTEGER),
+                                                            'views': Schema(type=TYPE_STRING),
+                                                        })
+                         }
+                         )
+    def getAvgUserRatings(self, user):
+        userRatings = Rating.objects.filter(author=user.id)
+        return userRatings.aggregate(Avg('rating'))['rating__avg']
+
+    def getSumUserRecipes(self, user):
+        userRecipes = Recipe.objects.filter(author=user.id)
+        return userRecipes.aggregate(Sum('view_count'))['view_count__sum']
+
+    def get(self, request, pk):
+        try:
+            user = User.objects.get(pk=pk)
+            data = {'name': user.username, 'rating': self.getAvgUserRatings(user),
+                    'views': self.getSumUserRecipes(user)}
+            serializer = UserProfileSerializer(data=data)
+        except (User.DoesNotExist, Rating.DoesNotExist, Recipe.DoesNotExist):
+            return JsonResponse({}, status=status.HTTP_404_NOT_FOUND)
+        try:
+            if serializer.is_valid(raise_exception=True):
+                return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+        except serializers.ValidationError as valEr:
+            return JsonResponse({'errorMessage': valEr.detail}, status=status.HTTP_400_BAD_REQUEST)
