9f4b7eef908c01f2085b6c14aa9901b6c6a0c835
Michal Jankowski <michaljankowskiReal@gmail.com>
2022-01-09 18:31:53 +0100

    Pass info to main app about images

--- recipesAPI/recipesAPI/urls.py
+++ recipesAPI/recipesAPI/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, re_path
 from django.conf.urls import include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
+from django.conf.urls.static import static
+from django.conf import settings
 from drf_yasg import openapi
 from recipesAPI import views
 
@@ -42,3 +44,4 @@ urlpatterns = [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
